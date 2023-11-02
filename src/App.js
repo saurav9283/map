d6eb3762183c322709031e8ca5ef2c7bbf612ff1
Saurav <sauravkumar00108@gmail.com>
2023-11-02 22:35:31 +0530

    changes on coordinates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import {MapContainer,TileLayer} from 'react-leaflet'
-const center = [30.92242502926363, 75.87046651406249]
+import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const center = [28.526563890713174, 77.11413256189118];
+
 function App() {
   return (
-    <MapContainer center={center} zoom={10} style={{width:'100vw' , height:'100vh'}}>
+    <MapContainer center={center} zoom={3} style={{ width: "100vw", height: "100vh" }}>
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Stamen Watercolor">
+          <TileLayer
+            attribution='&copy; <a href="http://maps.stamen.com/copyright">Stamen Design</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
+      </LayersControl>
     </MapContainer>
   );
 }
