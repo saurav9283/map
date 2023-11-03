78f090d4368e85c9b7476694e6304e87a20389c0
Saurav <sauravkumar00108@gmail.com>
2023-11-03 15:26:44 +0530

    changes in map link

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
         <LayersControl.BaseLayer checked name="OpenStreetMap">
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=qYXBEfnoy4nw81LPAzuv"
           />
         </LayersControl.BaseLayer>
         <LayersControl.BaseLayer name="Stamen Watercolor">
           <TileLayer
             attribution='&copy; <a href="http://maps.stamen.com/copyright">Stamen Design</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=qYXBEfnoy4nw81LPAzuv"
           />
         </LayersControl.BaseLayer>
       </LayersControl>
